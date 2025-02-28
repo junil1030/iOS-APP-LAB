92bbbc38c3de7a48a86d7f915f3ef5735f4f2693
JunilSeo <dccrdseo@naver.com>
2025-03-01 00:46:39 +0900

    feat: Nav&Tab Project

---
+++ Nav&Tab/Nav&Tab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav&Tab
+//
+//  Created by 서준일 on 3/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
